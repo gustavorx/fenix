ecd42f63195f86609293c57e3fe105e96b496413
Gustavo Navarro Vilela <gvilela@nvbt.com.br>
2025-05-23 16:09:25 -0300

    wip

--- src/api/Database/FenixContext.cs
+++ src/api/Database/FenixContext.cs
@@ -1,5 +1,13 @@
-﻿namespace api.Database;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class FenixContext
+namespace api.Database;
+
+public class FenixContext : DbContext
 {
+    public FenixContext(DbContextOptions<FenixContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Transaction> Transactions { get; set; } = null!;
+
 }
